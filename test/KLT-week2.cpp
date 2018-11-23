7fcdec3b4167e42fdc4d49f1db65a717112e8dd0
lpan25 <sophie.leipan@gmail.com>
2018-11-22 16:29:19 -0800

    Normalize

--- test/KLT-week2.cpp
+++ test/KLT-week2.cpp
@@ -73,7 +73,6 @@
 //    return 0;
 //}
 
-
 //
 // Created by sicong on 08/11/18.
 //
@@ -88,7 +87,6 @@
 #include <math.h>
 using namespace std;
 
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d/features2d.hpp>
@@ -96,101 +94,124 @@ using namespace std;
 
 using namespace cv;
 
-cv::Matx33d Findfundamental(vector<cv::Point2f> prev_subset,vector<cv::Point2f> next_subset){
-    cv::Matx33d F;
+cv::Matx33d Findfundamental(vector<cv::Point2f> prev_subset, vector<cv::Point2f> next_subset,int img_w, int img_h)
+{
     // TODO
     // normalize the points
+    cv::Mat norm = (Mat_<double>(3,3)<<2/img_w, 0, -1, 0, 2/img_h, -1, 0, 0, 1); 
 
     // get the points
-    num_vals = 9;
+    int num_vals = prev_subset.size() + 1;
     // 9 * 9, the last row should be 0
-    cv::Mat W = cv::Mat::zeros(prev_subset.size() + 1, num_vals, CV_32F);
-    for (size_t i = 0; i < prev_subset.size() + 1; i++) {
-        float u1 = prev_subset[i].x;
-        float v1 = prev_subset[i].y;
-        float u2 = next_subset[i].x;
-        float v2 = next_subset[i].y;
-        float curr_point[num_vals] = { u1 * u2, u1 * v2, u1, v1 * u2, v1 * v2, v1, u2, v2, 1.0 };
-        cv::Mat curr_row = cv::Mat(1, num_vals, CV_32F, curr_point);
-        curr_row.row(0).copyTo(W.row(i));
+    cv::Mat W = cv::Mat::zeros(prev_subset.size(), num_vals, CV_64F);
+    for (size_t i = 0; i < prev_subset.size(); i++)
+    {
+        cv::Mat prev = (Mat_<double>(3,1)<<prev_subset[i].x, prev_subset[i].y, 1); 
+        cv::Mat next = (Mat_<double>(3,1)<<next_subset[i].x, next_subset[i].y, 1); 
+        cv::Mat prev_norm = norm*prev;
+        cv::Mat next_norm = norm*next;
+        double u1 = prev_norm.at<double>(0,0);
+        double v1 = prev_norm.at<double>(1,0);
+        double u2 = next_norm.at<double>(0,0);
+        double v2 = next_norm.at<double>(1,0);
+        double curr_point[] = {u1 * u2, u1 * v2, u1, v1 * u2, v1 * v2, v1, u2, v2, 1.0};
+        cv::Mat curr_row = cv::Mat(1, num_vals, CV_64F, curr_point);
+        curr_row(0).copyTo(W.row(i));
     }
+    // first SVD
+    cv::SVD svd1(W); // get svd.vt, svd.w, svd.u;
 
-    cv::SVD svd(W); // get svd.vt, svd.w, svd.u;
+    // second SVD
+    cv::Mat e_hat;
+
+    for (int i = 0; i < 9; i++)
+    {
+        e_hat.at<double>(i%3, i/3) = svd1.vt.at<double>((num_vals - 1), i);
+    }
+    cv::SVD svd2(e_hat);
     cv::Mat w;
-    svd.w.copyTo(w);
-    w.at<float>(2) = 0;
+    svd2.w.copyTo(w);
+    cv::Mat w_hat = cv::Mat::zeros(3,3, CV_64F);
+    w_hat.at<double>(0, 0) = w.at<double>(0, 0);
+    w_hat.at<double>(1, 1) = w.at<double>(1, 0);
 
-    F = svd.u * w * svd.vt;
+    cv::Mat F_hat = svd2.u * w_hat * svd2.vt;
+    // TODO
+    // denormalize points
+    cv::Mat F_norm = norm.t()*F_hat*norm;
+    cv::Matx33d F((double *)F_norm.clone().ptr());
 
     return F;
 }
-bool checkinlier(cv::Point2f prev_keypoint,cv::Point2f next_keypoint,cv::Matx33d Fcandidate, double d){
-    // TODO
-    float u1 = prev_keypoint.x;
-    float v1 = prev_keypoint.y;
-    float u2 = next_keypoint.x;
-    float v2 = next_keypoint.y;
+bool checkinlier(cv::Point2f prev_keypoint, cv::Point2f next_keypoint, cv::Matx33d Fcandidate, double d)
+{
+    double u1 = prev_keypoint.x;
+    double v1 = prev_keypoint.y;
+    double u2 = next_keypoint.x;
+    double v2 = next_keypoint.y;
 
-    // epipolar line 2 to 1
+    // epipolar line 1 to 2
     cv::Matx33d Fcandidate_t = Fcandidate.t();
-    float a1 = Fcandidate[0] * u2 + Fcandidate[1] * v2 + Fcandidate[2];
-    float b1 = Fcandidate[3] * u2 + Fcandidate[4] * v2 + Fcandidate[5];
-    float c1 = Fcandidate[6] * u2 + Fcandidate[7] * v2 + Fcandidate[8];
+    double a2 = Fcandidate_t(0, 0) * u1 + Fcandidate_t(0, 1) * v1 + Fcandidate_t(0, 2);
+    double b2 = Fcandidate_t(1, 0) * u2 + Fcandidate_t(1, 1) * v2 + Fcandidate_t(1, 2);
+    double c2 = Fcandidate_t(2, 0) * u2 + Fcandidate_t(2, 1) * v2 + Fcandidate_t(2, 2);
 
-    double dist = (double) abs(a1 * u1 + b1 * v1 + c1) / sqrt(a1 * a1 + b1 * b1);
+    double dist = (double)abs(a2 * u1 + b2 * v1 + c2) / sqrt(a2 * a2 + b2 * b2);
     return dist <= d;
 }
 
-int main( int argc, char** argv )
+int main(int argc, char **argv)
 {
 
-    srand ( time(NULL) );
+    srand(time(NULL));
 
-    if ( argc != 3 )
+    if (argc != 3)
     {
-        cout<<"usage: feature_extraction img1 img2"<<endl;
+        cout << "usage: feature_extraction img1 img2" << endl;
         return 1;
     }
     //-- Read two images
-    Mat img_1 = imread ( argv[1], CV_LOAD_IMAGE_COLOR );
-    Mat img_2 = imread ( argv[2], CV_LOAD_IMAGE_COLOR );
+    Mat img_1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat img_2 = imread(argv[2], CV_LOAD_IMAGE_COLOR);
 
-    list< cv::Point2f > keypoints;
+    // Get input img size
+    int img_w = img_1.size().width;
+    int img_h = img_1.size().height;
+
+    list<cv::Point2f> keypoints;
     vector<cv::KeyPoint> kps;
 
     std::string detectorType = "Feature2D.BRISK";
-    Ptr<FeatureDetector>detector = Algorithm::create<FeatureDetector>(detectorType);
+    Ptr<FeatureDetector> detector = Algorithm::create<FeatureDetector>(detectorType);
     detector->set("thres", 100);
 
-
-    detector->detect( img_1, kps );
-    for ( auto kp:kps )
-        keypoints.push_back( kp.pt );
+    detector->detect(img_1, kps);
+    for (auto kp : kps)
+        keypoints.push_back(kp.pt);
 
     vector<cv::Point2f> next_keypoints;
     vector<cv::Point2f> prev_keypoints;
-    for ( auto kp:keypoints )
+    for (auto kp : keypoints)
         prev_keypoints.push_back(kp);
     vector<unsigned char> status;
     vector<float> error;
     chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
-    cv::calcOpticalFlowPyrLK( img_1, img_2, prev_keypoints, next_keypoints, status, error );
+    cv::calcOpticalFlowPyrLK(img_1, img_2, prev_keypoints, next_keypoints, status, error);
     chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>( t2-t1 );
-    cout<<"LK Flow use time："<<time_used.count()<<" seconds."<<endl;
+    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+    cout << "LK Flow use time：" << time_used.count() << " seconds." << endl;
 
-    vector<cv::Point2f> kps_prev,kps_next;
+    vector<cv::Point2f> kps_prev, kps_next;
     kps_prev.clear();
     kps_next.clear();
-    for(size_t i=0;i<prev_keypoints.size();i++)
+    for (size_t i = 0; i < prev_keypoints.size(); i++)
     {
-        if(status[i] == 1)
+        if (status[i] == 1)
         {
             kps_prev.push_back(prev_keypoints[i]);
             kps_next.push_back(next_keypoints[i]);
         }
     }
-
 
     // p Probability that at least one valid set of inliers is chosen
     // d Tolerated distance from the model for inliers
@@ -199,37 +220,41 @@ int main( int argc, char** argv )
     double d = 1.5f;
     double e = 0.2;
 
-    int niter = static_cast<int>(std::ceil(std::log(1.0-p)/std::log(1.0-std::pow(1.0-e,8))));
+    int niter = static_cast<int>(std::ceil(std::log(1.0 - p) / std::log(1.0 - std::pow(1.0 - e, 8))));
     Mat Fundamental;
-    cv::Matx33d F,Fcandidate;
+    cv::Matx33d F, Fcandidate;
     int bestinliers = -1;
-    vector<cv::Point2f> prev_subset,next_subset;
+    vector<cv::Point2f> prev_subset, next_subset;
     int matches = kps_prev.size();
     prev_subset.clear();
     next_subset.clear();
 
-    for(int i=0;i<niter;i++){
+    for (int i = 0; i < niter; i++)
+    {
         // step1: randomly sample 8 matches for 8pt algorithm
         unordered_set<int> rand_util;
-        while(rand_util.size()<8)
+        while (rand_util.size() < 8)
         {
             int randi = rand() % matches;
             rand_util.insert(randi);
         }
-        vector<int> random_indices (rand_util.begin(),rand_util.end());
-        for(size_t j = 0;j<rand_util.size();j++){
+        vector<int> random_indices(rand_util.begin(), rand_util.end());
+        for (size_t j = 0; j < rand_util.size(); j++)
+        {
             prev_subset.push_back(kps_prev[random_indices[j]]);
             next_subset.push_back(kps_next[random_indices[j]]);
         }
         // step2: perform 8pt algorithm, get candidate F
-        Fcandidate = Findfundamental(prev_subset,next_subset);
+
+        Fcandidate = Findfundamental(prev_subset, next_subset, img_w, img_h);
         // step3: Evaluate inliers, decide if we need to update the best solution
         int inliers = 0;
-        for(size_t j=0;j<kps_prev.size();j++){
-            if(checkinlier(kps_prev[j],kps_next[j],Fcandidate,d))
+        for (size_t j = 0; j < kps_prev.size(); j++)
+        {
+            if (checkinlier(kps_prev[j], kps_next[j], Fcandidate, d))
                 inliers++;
         }
-        if(inliers > bestinliers)
+        if (inliers > bestinliers)
         {
             F = Fcandidate;
             bestinliers = inliers;
@@ -240,16 +265,17 @@ int main( int argc, char** argv )
 
     // step4: After we finish all the iterations, use the inliers of the best model to compute Fundamental matrix again.
 
-    for(size_t j=0;j<kps_prev.size();j++){
-        if(checkinlier(kps_prev[j],kps_next[j],F,d))
+    for (size_t j = 0; j < kps_prev.size(); j++)
+    {
+        if (checkinlier(kps_prev[j], kps_next[j], F, d))
         {
             prev_subset.push_back(kps_prev[j]);
             next_subset.push_back(kps_next[j]);
         }
-
     }
-    F = Findfundamental(prev_subset,next_subset);
+    F = Findfundamental(prev_subset, next_subset);
 
-    cout<<"Fundamental matrix is \n"<<F<<endl;
+    cout << "Fundamental matrix is \n"
+         << F << endl;
     return 0;
 }
